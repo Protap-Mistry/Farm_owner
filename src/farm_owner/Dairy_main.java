1ca1dac8e060a0a49d6a7a88f3a7dbe2f9d51717
Protap-mistry <pro.cse4.bu@gmail.com>
2021-07-26 13:37:06 +0600

    first commit

---
+++ src/farm_owner/Dairy_main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farm_owner;
+
+/**
+ *
+ * @author PRO_
+ */
+public class Dairy_main 
+{
+    private int serial;
+    private String product_name;
+    private String quantity;
+    public Dairy_main(int Serial, String ProductName, String Quantity)
+    {
+        this.serial= Serial;
+        this.product_name= ProductName;
+        this.quantity= Quantity;
+    }
+    public int getSerial()
+    {
+        return serial;
+    }
+    public String getProductName()
+    {
+        return product_name;
+    }
+    public String getQuantity()
+    {
+        return quantity;
+    }
+}
